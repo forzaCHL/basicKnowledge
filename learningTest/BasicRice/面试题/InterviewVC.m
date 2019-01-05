cd94efcfa898e4c385b36ff9c1a2fe95a1961b6a
陈海林 <chenhailin@zhixueyun.com>
2019-01-05 10:07:50 +0800

    bendi

---
+++ learningTest/BasicRice/面试题/InterviewVC.m
@@ -0,0 +1,37 @@
+//
+//  InterviewVC.m
+//  learningTest
+//
+//  Created by Apple on 2018/6/8.
+//  Copyright © 2018年 Apple. All rights reserved.
+//
+
+#import "InterviewVC.h"
+
+@interface InterviewVC ()
+
+@end
+
+@implementation InterviewVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
